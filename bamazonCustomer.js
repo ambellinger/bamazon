4dd6b39d73192d1ccf3cb9d63c71fed92618f8e1
Lexi <ambellinger24@gmail.com>
2019-01-09 19:14:19 -0600

    inquirer up and running, now need to figure out how to update the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,153 +25,59 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  print()
-  //start();
+ start();
 });
 
 //Function to print out products
 
-function print() {
+ // function which prompts the user for what action they should take
+ function start() {
+
   connection.query("SELECT * FROM products", function(err, result, fields) {
-  if (err) throw err;
-  //console.log(result);
-  console.table(result);
-  
-  })
-}
+    if (err) throw err;
+    //console.log(result);
+    console.table(result);
 
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          message: "What is the ID of the item you would like to purchase? [Quit with Q]",
+          type: "input"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like? [Quit with Q]"
+        }
+      ])
+      .then(function(answer) {
+        var chosenId = answer.choice - 1;
+       // console.log("you chose item:" + chosenId);
+       var chosenAmount = answer.quantity;
+       console.log("the chosen amount is" + chosenAmount);
+       console.log(result[chosenId].product_name);
 
+       if (chosenAmount > result[chosenId].stock_quantity) {
+         console.log("Insufficient Quantity");
+       } else {
+         console.log("Yay boy");
+       }
+     //   var chosenItem;
 
-// // function which prompts the user for what action they should take
-// function start() {
-//   inquirer
-//     .prompt({
-//       name: "postOrBid",
-//       type: "rawlist",
-//       message: "Would you like to [POST] an auction or [BID] on an auction?",
-//       choices: ["POST", "BID"]
-//     })
-//     .then(function(answer) {
-//       // based on their answer, either call the bid or the post functions
-//       if (answer.postOrBid.toUpperCase() === "POST") {
-//         postAuction();
-//       }
-//       else {
-//         bidAuction();
-//       }
-//     });
-// }
-
-// // function to handle posting new items up for auction
-// function postAuction() {
-//   // prompt for info about the item being put up for auction
-//   inquirer
-//     .prompt([
-//       {
-//         name: "item",
-//         type: "input",
-//         message: "What is the item you would like to submit?"
-//       },
-//       {
-//         name: "category",
-//         type: "input",
-//         message: "What category would you like to place your auction in?"
-//       },
-//       {
-//         name: "startingBid",
-//         type: "input",
-//         message: "What would you like your starting bid to be?",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       // when finished prompting, insert a new item into the db with that info
-//       connection.query(
-//         "INSERT INTO auctions SET ?",
-//         {
-//           item_name: answer.item,
-//           category: answer.category,
-//           starting_bid: answer.startingBid,
-//           highest_bid: answer.startingBid
-//         },
-//         function(err) {
-//           if (err) throw err;
-//           console.log("Your auction was created successfully!");
-//           // re-prompt the user for if they want to bid or post
-//           start();
-//         }
-//       );
-//     });
-// }
-
-// function bidAuction() {
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-   
-//     //results of the sql select statement
-//    console.table(results);
-   
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             var choiceArray = [];
-//             for (var i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         var chosenItem;
-//         for (var i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
-
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
-// }
+        
+          // result[chosenId].item_id === answer.choice) {
+          //   chosenItem = result[i];
+          //   console.log(chosenItem);
+          //   console.log(result[i].product_name);
+          // }
+         // console.log(result[i].item_id);
+          //if (result[i].item_id === chosenId) {
+          //   chosenItem = result[i];
+          // }
+        
+        
+      })
+    
+    })
+  }
